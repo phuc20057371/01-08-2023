84361dc73d1c9796652d6b3495db587743b41f8b
catlynn0208 <foxfessor@gmail.com>
2023-08-01 15:49:08 +0700

    singleton

--- Singleton_example/src/main/java/app/App.java
+++ Singleton_example/src/main/java/app/App.java
@@ -4,6 +4,9 @@ import db.Connection;
 
 public class App {
 	public static void main(String[] args) {
-		Connection.getInstance();
+		Connection connection = Connection.getInstance();
+		Connection connection1 = Connection.getInstance();
+		System.out.println(connection);
+		System.out.println(connection1);
 	}
 }
